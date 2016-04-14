1eab8a70dfa5ac57418756aa58d668fa9d3f980d
Jim Lin <jimlintw922@gmail.com>
2016-04-15 05:09:44 +0800

    0.0.7

--- pp20160412212350/__main__.py
+++ pp20160412212350/__main__.py
@@ -5,18 +5,7 @@ import sys
 import argparse
 
 from . import helloworld
-
-
-def do_copy(args):
-    print "Copying", args.input, args.output
-
-    from shutil import copyfile
-    copyfile(args.input, args.output)
-
-
-def do_sum(args):
-    print "Calculating the sum of", args.number
-    print sum(args.number)
+from . import lib
 
 
 def main():
@@ -38,14 +27,14 @@ def main():
                              required=True)
     parser_copy.add_argument('-o', '--output', help='output file name',
                              required=True)
-    parser_copy.set_defaults(func=do_copy)
+    parser_copy.set_defaults(func=lib.cmdcopy.do_copy)
 
     # add subcommand - sum
     parser_sum = subparsers.add_parser('sum',
                                        help='calculate the sum of numbers')
     parser_sum.add_argument('number', metavar='N', type=int, nargs='+',
                             help='a list of numbers for sum operation')
-    parser_sum.set_defaults(func=do_sum)
+    parser_sum.set_defaults(func=lib.cmdsum.do_sum)
 
     # add subcommand - helloworld
     parser_helloworld = subparsers.add_parser('helloworld',
